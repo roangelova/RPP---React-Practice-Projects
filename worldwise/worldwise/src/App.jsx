5db721716dba2480e8bc6b3786c56f2009c8d278
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-02-27 14:30:41 +0200

    add nested routes and AppNav

--- worldwise/worldwise/src/App.jsx
+++ worldwise/worldwise/src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
 
@@ -16,6 +17,12 @@ function App() {
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />} >
+          <Route index element={<p> List of cities</p>} />
+          <Route path='cities' element={<p> List of cities</p>} />
+          <Route path='countries' element={<p> List of countries</p>} />
+          <Route path='form' element={<p>form</p>} />
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>

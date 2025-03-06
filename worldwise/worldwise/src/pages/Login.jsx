57de9dd04680cd71f81c53cd4772caa153cfa7a5
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-03-06 14:07:06 +0200

    finish section: add AuthContext and implement fake login/logout; add ProtectedRoute

--- worldwise/worldwise/src/pages/Login.jsx
+++ worldwise/worldwise/src/pages/Login.jsx
@@ -1,16 +1,38 @@
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/AuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/app', { replace: true })
+    }
+  }, [isAuthenticated, navigate])
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email === '' || password === '') {
+      alert('Password and email are required')
+      return;
+    }
+
+    login(email, password);
+  }
 
   return (
     <main className={styles.login}>
-      <PageNav/>
-      <form className={styles.form}>
+      <PageNav />
+      <form className={styles.form} onSubmit={handleLogin}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +54,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type='primary'>Login</Button>
         </div>
       </form>
     </main>

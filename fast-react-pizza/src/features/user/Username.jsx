9664f10fcca1e500196d6d84ddc036f27674ada3
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-03-11 17:31:43 +0200

    add wip: tailwindcss

---
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="hidden font-semibold test-sm">
+            Roslava
+        </div>
+      );
+}
+
+export default Username;
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
     return (
-        <header>
-            <Link to='/' >Fast React Pizza Co.</Link>
+        <header className="px-4 py-3 uppercase bg-yellow-500 border-b border-stone-200 sm:px-6">
+            <Link className="tracking-widest" to='/' >Fast React Pizza Co.</Link>
 
             <SearchOrder />
+
+            <Username/>
         </header>
     );
 }

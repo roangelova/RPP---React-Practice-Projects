4345281c3c164c15649f54f95c1240aeddef08b9
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-03-26 11:42:43 +0200

    finish chapter 25: build the layout and add styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,8 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+import GlobalStyles from './styles/GlobalStyles'
+
+import Dashboard from './pages/Dashboard'
+import Account from './pages/Account'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Settings from './pages/Settings'
+import Users from './pages/Users'
+import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import AppLayout from "./ui/AppLayout"
 
 function App() {
-  return
-  (
-    <p>Hello</p>
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='account' element={<Account />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='users' element={<Users />} />
+          </Route>
+
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- the-wild-oasis/src/ui/MainNav.jsx
+++ the-wild-oasis/src/ui/MainNav.jsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { HiOutlineCalendarDays, HiOutlineCog6Tooth, HiOutlineHome, HiOutlineHomeModern, HiOutlineUsers } from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
+  margin-top:4rem;
 `;
 
-const Link = styled.a`
+//pass a component
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +48,49 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to='/dashboard'>
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to='/bookings'>
+            <HiOutlineCalendarDays />
+            <span>Bookings</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to='/cabins'>
+            <HiOutlineHomeModern />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to='/users'>
+            <HiOutlineUsers />
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+
+        <li>
+          <StyledNavLink to='/settings'>
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
+        </li>
+
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;

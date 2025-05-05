0c8c272f14e09bf86011399c793455a6c2994cf1
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-05-05 17:09:16 +0300

    add error boundary and not-found.js; add static params generation

--- the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,17 +1,26 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-export async function generateMetadata({params}){
-  const {name} = await getCabin(params.cabinId);
-  return {title: `Cabin ${name}`}
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `Cabin ${name}` }
 }
 
-export default async function Page({params}) {
+//make NEXT.JS aware of all the possible ids since we know them beforehand
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map(cabin => ({ cabinId: String(cabin.id) }));
+
+  return ids;
+}
+
+export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } = cabin;
-    
+
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">

6bca60ae311fd603a72a5c5702addf1ea50f869c
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-06-06 11:42:12 +0300

    fix .env bug; add deleteBooking; add useFormStatus when submitting edity user data req; allow only future bookings to be edited and deleted;

--- the-wild-oasis-website/app/_components/Form.js
+++ the-wild-oasis-website/app/_components/Form.js
@@ -1,19 +1,24 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
 import { updateProfile } from "../_lib/actions";
 
 //THE ONLY WAY TO RENDER A SERVER COMPONENT INTO A SERVER COMPONENT IS TO PASS IT AS A PROP!
 
+//we cant use the useFormStatus in a component that contains the form!!
 function Form({ guest, children }) {
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
-    <form action={updateProfile} className="flex flex-col gap-6 px-12 py-8 text-lg bg-primary-900">
+    <form
+      action={updateProfile}
+      className="flex flex-col gap-6 px-12 py-8 text-lg bg-primary-900"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
-          name='fullName'
+          name="fullName"
           defaultValue={fullName}
           className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -23,7 +28,7 @@ function Form({ guest, children }) {
         <label>Email address</label>
         <input
           disabled
-          name='email'
+          name="email"
           defaultValue={email}
           className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -34,7 +39,7 @@ function Form({ guest, children }) {
           <label htmlFor="nationality">Where are you from?</label>
           <img
             src={countryFlag}
-            name='countryFlag'
+            name="countryFlag"
             alt="Country flag"
             className="h-5 rounded-sm"
           />
@@ -53,11 +58,23 @@ function Form({ guest, children }) {
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+//MUST BE INSIDE A COMPONENT THAT IS RENDERED INSIDE!! the form
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating.." : "Update profile"}
+    </button>
   );
 }
 
--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -4,6 +4,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
     await signIn("google", { redirectTo: '/account' })
@@ -41,4 +42,56 @@ export async function updateProfile(formData) {
 
     //browser stores the data ot dynamic pages for 30 seconds
     revalidatePath("/account/profile"); //-> manual on demand revalidation
+}
+
+export async function deleteBooking(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in!");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted!");
+
+  revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get("bookingId"));
+
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) throw new Error("Booking could not be updated");
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  redirect("/account/reservations");
 }

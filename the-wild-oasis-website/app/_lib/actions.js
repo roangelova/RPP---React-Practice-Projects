5db17d8cce0f68430f45caa1beed251e901f9eb2
Roslava Tsoncheva <88967550+roangelova@users.noreply.github.com>
2025-06-06 10:02:12 +0300

    add notes

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -1,5 +1,5 @@
 //kinda a convention to have our server actions here
-"use server"
+"use server" // -> they would run exclusively on the server; a bridge between the server and the clienet; 
 
 import { signIn, signOut } from "./auth"
 
@@ -10,3 +10,8 @@ export async function signInAction(){
 export async function SignOutAction(){
     await signOut({redirectTo: '/'})
 }
+
+//Next.js creates an API endpoint with URL for each server action; A post request is made to that URL, but the function NEVER reaches the client,
+//meaning that the code always stays on the server and is never exposed to the browser; Therefore, it is technically safe to expose API keys and connect direct to a db etc. 
+//!!!Unlike server components, server actions actually require a running web server; 
+//they could also be called from event handlers and useEffect; 
